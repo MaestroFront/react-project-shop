3c417d6ee7c009f06579403af4d6bd3a8b405058
Maestro <akievaleksandr@mail.ru>
2024-03-06 20:31:26 +0500

    fix start settings and add module scss + add font

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Footer.module.scss";
 
 export default function Footer() {
     return <footer className="footer">Footer &copy;</footer>;
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Main.module.scss";
 
 export default function Main() {
     return <main className="main">Main</main>;
